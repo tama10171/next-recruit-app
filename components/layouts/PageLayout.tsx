62c411f08ba869c83dad73e85a8c153ba7a65214
tama10171 <tama10171nnm@gmail.com>
2023-01-21 10:58:37 +0900

    PageLayout作成

---
+++ components/layouts/PageLayout.tsx
@@ -0,0 +1,5 @@
+import { Box } from "@chakra-ui/react"
+
+export const PageLayout = ({children}:any) => {
+  return <Box bg={"#FFE7A9"}>{children}</Box>
+}
--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,10 +1,11 @@
+import { PageLayout } from "@/components/layouts/PageLayout"
 import { Button } from "@chakra-ui/react"
 
 export const SignIn = () => {
   return (
-    <>
+    <PageLayout>
       <div>SignIn</div>
       <Button colorScheme="blue">Button</Button>
-    </>
+    </PageLayout>
   )
 }
