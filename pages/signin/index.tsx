9c22bb335794229c9b26256f57d27074bfbe255b
Nora <nsorym04@gmail.com>
2023-01-21 12:17:25 +0900

    sassの導入

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,24 +1,27 @@
 import { PageLayout } from '@/components/layouts/PageLayout'
-import { Box, Button, Center, FormControl, FormLabel, HStack, Input, Text } from '@chakra-ui/react'
+import { AlertDialogBody, Box, Button, Center, FormControl, FormLabel, HStack, Input, Text } from '@chakra-ui/react'
+// import "../../styles/style.css"
 
 export const SignIn = () => {
   return (
     <PageLayout>
-      <Box width={'1268px'} bg="white" padding={'50px'}>
+      <div className="login_flex">
+      <Box className='login_box' bg="white">
         <HStack>
           <Text>学籍番号</Text>
-          <FormControl isRequired width={'704px'}>
+          <FormControl isRequired width={'70%'}>
             <Input placeholder="First name" />
           </FormControl>
         </HStack>
         <HStack>
           <Text>パスワード</Text>
-          <FormControl isRequired width={'704px'}>
+          <FormControl isRequired width={'70%'}>
             <Input placeholder="First name" />
           </FormControl>
         </HStack>
       </Box>
-      <Button colorScheme="blue">Button</Button>
+      <Button className='login_btn' colorScheme="blue">Button</Button>
+      </div>
     </PageLayout>
   )
 }
