b69d7c53d5c3dd68b7f1e15609067921965a3b45
Nora <nsorym04@gmail.com>
2023-01-21 23:18:30 +0900

    ヘッダー更新

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,20 +17,94 @@ import { UtilColors } from 'src/colors'
 export const Header = () => {
   return (
     <div>
-      <HStack w={'100vw'} h={'60px'} bg={UtilColors.background.main}>
-        <Link href="/">
-          <Button>活動一覧</Button>
-        </Link>
-        <Link href="/">
-          <Button>登録・変更</Button>
-        </Link>
-        <Link href="/">
-          <Button>グッドJOB</Button>
-        </Link>
-        <Link href="/">
-          <Button>企業検索</Button>
-        </Link>
-      </HStack>
+      <Box w={'100vw'} h={'60px'} bg={UtilColors.background.main}>
+        <HStack w={'auto'} h={'100%'} justifyContent={'flex-end'} mr={'8px'}>
+          <Link href="/">
+            <Button
+              bg={'#fff'}
+              w={'90px'}
+              h={'40px'}
+              fontSize={'1.4rem'}
+              borderRadius={'5px'}
+              _after={{
+                pos: 'absolute',
+                top: '2.5px',
+                left: '2.5px',
+                w: 'calc(100% - 5.5px)',
+                h: 'calc(100% - 5.5px)',
+                border: '#FFB800 2px solid',
+                borderRadius: '5px',
+                content: `""`,
+              }}
+            >
+              活動一覧
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button
+              bg={'#fff'}
+              w={'90px'}
+              h={'40px'}
+              fontSize={'1.4rem'}
+              borderRadius={'5px'}
+              _after={{
+                pos: 'absolute',
+                top: '2.5px',
+                left: '2.5px',
+                w: 'calc(100% - 5.5px)',
+                h: 'calc(100% - 5.5px)',
+                border: '#FFB800 2px solid',
+                borderRadius: '5px',
+                content: `""`,
+              }}
+            >
+              登録・変更
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button
+              bg={'#fff'}
+              w={'90px'}
+              h={'40px'}
+              fontSize={'1.4rem'}
+              borderRadius={'5px'}
+              _after={{
+                pos: 'absolute',
+                top: '2.5px',
+                left: '2.5px',
+                w: 'calc(100% - 5.5px)',
+                h: 'calc(100% - 5.5px)',
+                border: '#FFB800 2px solid',
+                borderRadius: '5px',
+                content: `""`,
+              }}
+            >
+              グッドJOB
+            </Button>
+          </Link>
+          <Link href="/">
+            <Button
+              bg={'#fff'}
+              w={'90px'}
+              h={'40px'}
+              fontSize={'1.4rem'}
+              borderRadius={'5px'}
+              _after={{
+                pos: 'absolute',
+                top: '2.5px',
+                left: '2.5px',
+                w: 'calc(100% - 5.5px)',
+                h: 'calc(100% - 5.5px)',
+                border: '#FFB800 2px solid',
+                borderRadius: '5px',
+                content: `""`,
+              }}
+            >
+              企業検索
+            </Button>
+          </Link>
+        </HStack>
+      </Box>
     </div>
   )
 }
