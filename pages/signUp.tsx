037d60f9da7fd810315bb4ff63d60929b6d28ede
tama10171 <tama10171nnm@gmail.com>
2023-01-21 17:32:13 +0900

    signinロジック作成

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -16,40 +16,12 @@ import {
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
-export const SignIn = () => {
+const SignUp = () => {
   const [email, setEmail] = useState('')
   const [schoolNumber, setSchoolNumber] = useState(0)
   const [password, setPassword] = useState('')
   const router = useRouter()
   const { user, setUser } = useUser()
-
-  const onsubmit = async () => {
-    // console.log('email', email)
-    // console.log('password', password)
-    // 学籍番号から、それに応じたuserのデータを取得する。
-    // const { data, error } = await supabase
-    //   .from('profiles')
-    //   .select('*')
-    //   .eq('school_number', email)
-    //   .single()
-    // console.log(data)
-    // 取得したデータのパスワードをもとに、入力されたパスワードと比較する
-    // const dbPassword = data && data.password
-    // if (dbPassword === password) {
-    //   console.log('get')
-    //   const { data, error } = await supabase.auth.signInWithOtp({
-    //     email: 'tama10171nnm@gmail.com',
-    //   })
-    //   router.push('/')
-    // }
-    //　比較して、trueなら、取得したuserのデータをsetして、/に遷移する。
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    })
-    if (error) console.log(error)
-    router.push('/')
-  }
 
   const onSignUp = async () => {
     const { data, error } = await supabase.auth.signUp({
@@ -57,7 +29,7 @@ export const SignIn = () => {
       password: password,
       options: {
         data: {
-          school_number: 28,
+          school_number: 0,
         },
       },
     })
@@ -68,6 +40,21 @@ export const SignIn = () => {
     <PageLayout>
       <div className="login_flex">
         <Box className="login_box" bg="white">
+          <HStack className="login_pare">
+            <Text className="login_form">学籍番号</Text>
+            {/* <Text className="login_form">学籍番号</Text> */}
+            <FormControl isRequired width={'70%'}>
+              <Input
+                h={'30px'}
+                bgColor={'#FFFAE8'}
+                border={'#FFB800 1px solid'}
+                fontSize={'1.6rem'}
+                onChange={(e) => {
+                  setSchoolNumber(Number(e.target.value))
+                }}
+              />
+            </FormControl>
+          </HStack>
           <HStack className="login_pare">
             <Text className="login_form">メールアドレス</Text>
             {/* <Text className="login_form">学籍番号</Text> */}
@@ -98,10 +85,11 @@ export const SignIn = () => {
             </FormControl>
           </HStack>
         </Box>
-        <Button className="login_btn" onClick={onsubmit}>
-          ログイン
+        <Button className="login_btn" onClick={onSignUp}>
+          新規登録
         </Button>
       </div>
     </PageLayout>
   )
 }
+export default SignUp
