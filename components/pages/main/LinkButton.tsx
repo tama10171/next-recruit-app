fcaa429b95582af9341312b86a1bdae70507c8e0
Nora <nsorym04@gmail.com>
2023-01-21 23:26:24 +0900

    メインボタンのデザインを更新

--- components/pages/main/LinkButton.tsx
+++ components/pages/main/LinkButton.tsx
@@ -26,11 +26,12 @@ export const LinkButton = (props: LinkButtonProps) => {
       <Button
         m={'10%'}
         w={'250px'}
-        _hover={{ bg: '#AA8800' }}
+        _hover={{ bg: '#dd8800', transform: 'translate(5px,5px)', boxShadow: '0px 0px 0px 0px' }}
         fontSize={'20px'}
         borderRadius={'10px'}
         bg={'#FFB800'}
         padding={'40px 0'}
+        boxShadow={'5px 5px 10px rgba(0, 0, 0, 0.542)'}
         _after={{
           pos: 'absolute',
           w: 'calc(100% - 7px)',
