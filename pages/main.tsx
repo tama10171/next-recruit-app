6e0c8a61e2074e77491d8e9e238aebe7a42fa9d3
Nyarlathotep1000 <itmmmzj6rc@i.softbank.jp>
2023-02-02 10:52:01 +0900

    活動登録ボタンのリンクをactivityChangeに変更

--- pages/main.tsx
+++ pages/main.tsx
@@ -6,7 +6,7 @@ const MainPage = () => {
     <PageLayout>
       <Center h={'calc(100vh - 60px)'}>
         <SimpleGrid columns={2} spacing={{ base: '10px 30px', md: '20px 50px' }} m={'0 auto'}>
-          <LinkButton info="活動登録" href={'/main'} ml={'-10%'} />
+          <LinkButton info="活動登録" href={'/activityChange'} ml={'-10%'} />
           <LinkButton info="活動一覧" href={'/recruit'} ml={'-10%'} />
           <LinkButton info="グッドJOB" href={'/news'} ml={'-10%'} />
           <LinkButton info="企業検索" href={'http://intra.denpa.ac.jp/prog/career/company.php'} ml={'-10%'} />
